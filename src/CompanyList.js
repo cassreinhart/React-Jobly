f9cf07d8b729218be51682e22c346dae8ca550d5
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2023-08-19 14:33:34 -0500

    company list search functionality

--- src/CompanyList.js
+++ src/CompanyList.js
@@ -1,18 +1,38 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import SearchBar from './SearchBar'
 import Company from './Company'
+import JoblyApi from '../api'
 import useFetch from './hooks/useFetch'
 
 const CompanyList = () => {
+    const [companies, setCompanies] = useState(null)
+
+    const getCompanies = () => {
+        search()
+    }
+
+    useEffect(getCompanies, [])
+
+    const search = async () => {
+        let companies = await JoblyApi.getCompanies(name);
+        setCompanies(companies);
+    }
   return (
     <div className='CompanyList'>
-        {isLoading ? 
+        {!companies ? 
             (<h4>Loading...</h4>)
             :
             (<div>
-                <SearchBar />
+                <SearchBar searchFor={search}/>
                 <ul>
-                    {companies.forEach(c => <Company key={c.handle} props={c}/>)}
+                    {companies.map(c => (
+                        <Company 
+                            key={c.handle} 
+                            handle={c.handle}
+                            name={c.name}
+                            description={c.description}
+                            logo={c.logoUrl}
+                        />))}
                 </ul>
             </div>)
         }
